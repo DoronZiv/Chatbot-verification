7a0440eae91e7e5eb670459fc438c5383f28d112
DoronZiv <doronzivai4u@gmail.com>
2025-04-07 13:59:52 +0300

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -37,7 +37,7 @@ export const ImageUploadExtension = {
   
         // Show appropriate loading indicator based on file type
         if (file.type.startsWith('image/')) {
-          fileUploadContainer.innerHTML = `<img src="https://com.voiceflow.studio/share/upload/upload.gif" alt="Upload" width="50" height="50">`
+          fileUploadContainer.innerHTML = `<img src="https://doronziv.github.io/Chatbot-verification/share/upload/upload.gif" alt="Upload" width="50" height="50">`
         } else {
           fileUploadContainer.innerHTML = `<div class="my-file-upload">Uploading text file...</div>`
         }
